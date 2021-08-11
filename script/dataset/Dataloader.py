e8536447d88f930b1d961d0ee0e8e9ea3310e7bf
Brian Lee <bobolee1239@gmail.com>
2021-08-11 22:00:58 +0800

    Implement Dataset and Dataloader for training

---
+++ script/dataset/Dataloader.py
@@ -0,0 +1,62 @@
+# ----------------------------------
+# File: Dataloader.py
+# ----------------------------------
+
+from torch.utils.data    import DataLoader, ConcatDataset
+from VoiceCommandDataset import VoiceCommandDataset
+
+TRAIN_SET_FOLDER = [
+    '/Users/brian/brian_ws/KWS/src/levoice/test_audio'
+]
+
+def get_train_dataloader():
+    trainsets = [VoiceCommandDataset(d) for d in TRAIN_SET_FOLDER]
+    trainset = ConcatDataset(trainsets)
+
+    loader = DataLoader(
+                trainset, 
+                batch_size=10, 
+                shuffle=True
+             )
+    return loader
+
+
+# -----------------------------------------------
+
+if __name__ == '__main__':
+    import pdb
+    import numpy as np
+    import librosa
+    import matplotlib.pyplot as plt
+
+    loader = get_train_dataloader()
+
+    def plot_feature1(feat):
+        import librosa.display
+        fig, ax = plt.subplots()
+        S_dB = 10.0 * np.log10(feat + 1e-12)
+        img = librosa.display.specshow(S_dB, x_axis='time',
+                         y_axis='mel', sr=16000,
+                         fmax=8000, ax=ax)
+        fig.colorbar(img, ax=ax, format='%+2.0f dB')
+        ax.set(title='Mel-frequency spectrogram')
+
+    def plot_feature2(feat, gamma=0.2):
+        cmap = 'inferno'
+        fig, ax = plt.subplots()
+        S_dB = feat ** gamma
+        ax.imshow(S_dB, origin='lower', cmap=cmap)
+        ax.set(title='Mel-frequency spectrogram')
+
+    for n, batch in enumerate(loader):
+        if n != 0:
+            break
+            
+        sigs, specs, mel_specs, label = batch
+
+        mel_spec = mel_specs[0].permute(1, 0).numpy()
+        spec = np.abs(specs[0].permute(1, 0).numpy()) ** 2
+        plot_feature2(mel_spec)
+        plot_feature2(spec)
+
+        pdb.set_trace()
---
+++ script/dataset/VoiceCommandDataset.py
@@ -0,0 +1,97 @@
+# ----------------------------------
+# File: VoiceCommandDataset.py
+# ----------------------------------
+import os 
+import random
+import torch
+import librosa
+
+import numpy     as np 
+import soundfile as sf
+
+from pathlib          import Path
+from torch.utils.data import Dataset
+
+SR       = 16000
+NFFT     = 1024
+NMEL     = 40
+HOP_SIZE = 160
+WIN_SIZE = 640
+WIN_TYPE = 'hann'
+
+SECONDS  = 2
+LENGTH   = SECONDS * SR
+# -----------------------------------------------------
+def pad_to(sig, target_len):
+    length = sig.shape[0]
+
+    if length >= target_len:
+        return sig[:target_len]
+    
+    pad_len = target_len - length
+
+    return np.concatenate((sig, np.zeros((pad_len, ))))
+
+
+def audioread_mono(wavfile):
+    sig, sr = librosa.load(wavfile, sr=SR, dtype=np.float32)
+
+    if len(sig.shape) > 1:
+        sig = sig[:, 0]
+
+    return sig, sr
+
+
+def stft(sig):
+    spectra = librosa.stft(
+                sig, 
+                n_fft=NFFT, 
+                hop_length=HOP_SIZE, 
+                win_length=WIN_SIZE, 
+                window=WIN_TYPE, 
+                center=False
+              )
+    return spectra
+
+
+class VoiceCommandDataset(Dataset):
+    def __init__(self, folder):
+        wavs = list(Path(folder).rglob('*.wav'))
+        wavs = [str(wav) for wav in wavs]
+
+        self.wavs   = wavs
+        self.length = len(wavs)
+
+        random.shuffle(self.wavs)
+    
+    def __len__(self):
+        return self.length
+    
+    def __getitem__(self, idx):
+        wavfile = self.wavs[idx]
+        label = int(os.path.basename(wavfile).split('_')[0])
+        sig, sr = audioread_mono(wavfile)
+
+        sig = pad_to(sig, LENGTH)
+
+        spectrum = stft(sig)
+        spectra  = np.abs(spectrum) ** 2.0
+
+        mel_spectra = librosa.feature.melspectrogram(
+                        #  S=spectra,
+                         y=sig,
+                         n_mels=NMEL,
+                         sr=SR, 
+                         fmax=SR/2,
+                         n_fft=NFFT, 
+                         hop_length=HOP_SIZE, 
+                         win_length=WIN_SIZE, 
+                         window=WIN_TYPE, 
+                         center=False, 
+                         power=2.0
+                       )
+        
+        # (nFrm, nFreq)
+        spectrum    = np.transpose(spectrum)
+        mel_spectra = np.transpose(mel_spectra)
+        return sig, spectrum, mel_spectra, label
